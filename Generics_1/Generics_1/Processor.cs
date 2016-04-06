ac9475cce637d6f71b69fd464990dd71779cff19
Mikhail Ivchenko <ematirov@gmail.com>
2016-04-06 22:05:03 +0400

    Initial commit.

---
+++ Generics_1/Generics_1/Processor.cs
@@ -0,0 +1,26 @@
+﻿namespace Generics_1
+{
+    public class Processor<TEngine, TEntity, TLogger>
+    {
+        public static ProcessorBuilderEntity<TCustomEngine> CreateEngine<TCustomEngine>()
+        {
+            return new ProcessorBuilderEntity<TCustomEngine>();
+        }
+    }
+
+    public class ProcessorBuilderEntity<TCustomEngine>
+    {
+        public ProcessorBuilderLogger<TCustomEngine, TCustomEntity> For<TCustomEntity>()
+        {
+            return new ProcessorBuilderLogger<TCustomEngine, TCustomEntity>();
+        }
+    }
+
+    public class ProcessorBuilderLogger<TEngine, TEntity>
+    {
+        public Processor<TEngine, TEntity, TLogger> With<TLogger>()
+        {
+            return new Processor<TEngine, TEntity, TLogger>();
+        } 
+    }
+}
---
+++ Generics_2/Generics_2/Repository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Generics_2
+{
+    public class Repository
+    {
+        private readonly Dictionary<Guid, object> _repository = new Dictionary<Guid, object>();
+        private readonly Dictionary<Type, List<Guid>> _guidsByType = new Dictionary<Type, List<Guid>>();
+
+        public TObject Create<TObject>()
+            where TObject : new()
+        {
+            var obj = new TObject();
+            var guid = Guid.NewGuid();
+            var type = typeof(TObject);
+
+            _repository.Add(guid, obj);
+
+            if( !_guidsByType.ContainsKey(type))
+                _guidsByType[type] = new List<Guid>();
+            _guidsByType[type].Add(guid);
+
+            return obj;
+        }
+
+        public IEnumerable<Tuple<Guid, TObject>> All<TObject>()
+        {
+            return _guidsByType[typeof (TObject)]
+                .Select(x => new Tuple<Guid, TObject>(x, (TObject) _repository[x]));
+        }
+
+        public TObject Get<TObject>(Guid guid)
+        {
+            if (!_repository.ContainsKey(guid) || !(_repository[guid] is TObject))
+                return default(TObject);
+            return (TObject) _repository[guid];
+        }
+    }
+}
